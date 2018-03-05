88df40b51f3fa646e11fd0ee838263e37b4a07b4
Abdelrahman Shiddo <ashiddo11@gmail.com>
2018-03-05 19:11:44 +0400

    get vpc

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
         "log"
         "time"
         "k8s.io/client-go/rest"
-        v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
         v1beta1 "k8s.io/api/extensions/v1beta1"
         extv1 "k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
         conf "github.com/ashiddo11/k8s-custom-hpa/config"
@@ -15,50 +14,45 @@ import (
 
 func scale(client extv1.DeploymentInterface, deployment string, deploymentConfig *v1beta1.Scale) (result *v1beta1.Scale) {
 
-	result, err := client.UpdateScale(deployment, deploymentConfig)
-	if err != nil {
-		log.Printf(err.Error())
-	}
+        result, err := client.UpdateScale(deployment, deploymentConfig)
+        if err != nil {
+                log.Printf(err.Error())
+        }
 
-	if err == nil {
-		log.Printf("Scaled %s to %d replicas" , deployment ,result.Spec.Replicas)
-	}
-	return
+        if err == nil {
+                log.Printf("Scaled %s to %d replicas" , deployment ,result.Spec.Replicas)
+        }
+        return
 }
 
 func main() {
 
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
+        config, err := rest.InClusterConfig()
+        if err != nil {
+             panic(err.Error())
+        }
 
-	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+        // create the clientset
+        clientset, err := kubernetes.NewForConfig(config)
+        if err != nil {
+                panic(err.Error())
+        }
 
         for {
                 appConfig, _ := conf.LoadConfig()
                 for _, app := range appConfig.Apps {
                         log.Printf("Checking %s", app.Name)
-                        deploymentsClient := clientset.ExtensionsV1beta1().Deployments(app.Namespace)
-                        deploymentScale, err := deploymentsClient.GetScale(app.Name, v1.GetOptions{})
-                        if err != nil {
-                                log.Println("[WARNING]", err)
-                                continue
-                        }
+                        deploymentScale, ns := util.GetDeployment(clientset, app.Name)
                         triggered := util.CheckMetric(app.Query)
                         switch {
                         case triggered && deploymentScale.Spec.Replicas < app.MaxReplicas:
                                 log.Printf("Scaling up %s", app.Name)
                                 deploymentScale.Spec.Replicas = int32(math.Min(float64(app.MaxReplicas), float64(deploymentScale.Spec.Replicas + app.ScaleFactor)))
-                                scale(deploymentsClient, app.Name, deploymentScale)
+                                util.ScaleDeployment(clientset, ns, app.Name, deploymentScale)
                         case !triggered && deploymentScale.Spec.Replicas > app.MinReplicas:
                                 log.Printf("Scaling down %s", app.Name)
                                 deploymentScale.Spec.Replicas = int32(math.Max(float64(app.MinReplicas), float64(deploymentScale.Spec.Replicas - app.ScaleFactor)))
-                                scale(deploymentsClient, app.Name, deploymentScale)
+                                util.ScaleDeployment(clientset, ns, app.Name, deploymentScale)
                         case triggered && deploymentScale.Spec.Replicas >= app.MaxReplicas:
                                 log.Printf("Reached maximum replicas, can't scale up anymore")
                         default:
